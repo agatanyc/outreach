ce02cf2c103b1a56b3ef7382902864a05d02b598
Agata Filho <agata78@gmail.com>
2014-08-03 08:54:02 -0400

    Plug data into template.

--- outreach.py
+++ outreach.py
@@ -1,4 +1,37 @@
 """Generates email from a template and a list of contacts."""
 
+#   inputs:
+#       contact info
+#       template
+#   outputs:
+#       email to send
+#       script
+
+from pprint import pprint
+import sys
+
+def slurp(name):
+    """(str) -> str"""
+    with open(name) as file:
+        return file.read()
+
+def spit(name, text):
+    """(str, str) -> NoneType"""
+    with open(name, 'w') as file:
+        file.write(text)
+
+def process(form, data):
+    template = slurp(form)
+    contacts = slurp(data)
+    for line in contacts.splitlines():
+        name, last, mail = line.split()
+        pprint((name, last, mail))
+        output = template.format(name=name, last=last, mail=mail)
+        pprint(output)
+        spit(mail, output)
+
 if __name__ == '__main__':
-	pass
+    if len(sys.argv) != 3:
+        print('usage: outreach.py <form> <data>')
+        sys.exit(1)
+    process(sys.argv[1], sys.argv[2])
